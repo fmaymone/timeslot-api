f83912cddc3cf3400ddc63ef8ad8b10a0325bc64
silvio <silvio@timeslot.com>
2014-10-08 13:09:37 +0200

    IMP testing of the models

--- spec/models/slot_spec.rb
+++ spec/models/slot_spec.rb
@@ -9,11 +9,27 @@ RSpec.describe Slot, type: :model do
 
   it { should respond_to(:visibility) }
   it { should respond_to(:alerts) }
-  it { should respond_to(:baseslot) }
   # it { should respond_to(:reslots) }
   # it { should respond_to(:comments) }
   # it { should respond_to(:likes) }
+  it { should respond_to(:baseslot) }
+  it { should belong_to(:baseslot).inverse_of(:slots) }
 
   it { should be_valid }
+
+  describe 'when baseslot is not present' do
+    before { @slot.baseslot = nil }
+    it { should_not be_valid }
+  end
+
+  describe 'when visibility is not present' do
+    before { @slot.visibility = '' }
+    it { should_not be_valid }
+  end
+
+  describe 'when alerts is not present' do
+    before { @slot.alerts = '' }
+    it { should_not be_valid }
+  end
 
 end
