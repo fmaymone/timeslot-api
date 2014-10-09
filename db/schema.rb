400e645eb33b226ca3131fb4f584f73bfe6ebb97
silvio <silvio@timeslot.com>
2014-10-09 15:51:39 +0200

    DEL removing unnecessary migrations

--- db/schema.rb
+++ db/schema.rb
@@ -11,25 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141007114115) do
+ActiveRecord::Schema.define(version: 20141007095331) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "baseslots", force: true do |t|
-    t.string   "title"
-    t.datetime "startdate"
-    t.datetime "enddate"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
   create_table "slots", force: true do |t|
     t.integer  "baseslot_id"
-    t.integer  "visibility",             default: 0
     t.string   "alerts",      limit: 10
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "visibility"
   end
 
 end
