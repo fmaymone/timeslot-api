77d6ae1ccd724169399d16e19be4c7e5540a876a
silvio <silvio@timeslot.com>
2014-10-09 15:18:08 +0200

    ADD routes for slots controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+
+  namespace :v1, defaults: { format: :json } do
+    resources :slots
+  end
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
