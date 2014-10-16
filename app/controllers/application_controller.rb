3f515f9c5d483b6f3425c63919473a6dde815521
silvio <silvio@timeslot.com>
2014-10-17 00:29:14 +0200

    ADD exception handling for invalid pg data

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,8 @@ class ApplicationController < ActionController::API
   rescue_from ActiveRecord::RecordNotFound do
     head :not_found
   end
+
+  rescue_from ActiveRecord::StatementInvalid do |exception|
+    render json: exception.message, status: :unprocessable_entity
+  end
 end
