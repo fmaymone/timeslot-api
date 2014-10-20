1aa5e5895956d6088fac5e92a811948d37e93a45
silvio <silvio@timeslot.com>
2014-10-20 13:41:27 +0200

    ADD migration for media_items

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141018114828) do
+ActiveRecord::Schema.define(version: 20141020105135) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "media_items", force: true do |t|
+    t.integer  "slot_id"
+    t.string   "media_type"
+    t.string   "public_id"
+    t.integer  "ordering"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "slots", force: true do |t|
     t.string   "title",      limit: 48
