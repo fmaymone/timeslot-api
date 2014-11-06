ea2ec22128550a4a43a74afa0bd2fa812139053d
silvio <silvio@timeslot.com>
2014-11-06 02:06:40 +0100

    WIP endpoints for groups

--- app/controllers/v1/groups_controller.rb
+++ app/controllers/v1/groups_controller.rb
@@ -2,12 +2,15 @@ module V1
   class GroupsController < ApplicationController
     # GET /v1/groups
     def index
+      # what to do with this?
+      # return all lists ???
+      # return all groups where the current user is member or owner ???
       @groups = Group.all
 
       render :index
     end
 
-    # GET /v1/groups/1
+    # GET /v1/groups/:group_id
     def show
       @group = Group.find(params[:id])
 
@@ -25,7 +28,8 @@ module V1
       end
     end
 
-    # PATCH /v1/groups/1
+    # PATCH /v1/groups/:group_id
+    # change name, image, owner, subs_can - states
     def update
       @group = Group.find(params[:id])
 
@@ -36,11 +40,74 @@ module V1
       end
     end
 
-    # DELETE /v1/groups/1
+    # DELETE /v1/groups/:group_id
     def destroy
       @group = Group.find(params[:id])
 
       render :show if SoftDeleteService.call(@group)
+    end
+
+    # GET /v1/groups/:group_id/members
+    # GET /v1/groups/members
+    # returns a list of all members of the specified group ordered by state
+    def members
+    end
+
+    # GET /v1/groups/:group_id/members/:user_id
+    # GET /v1/groups/members/show
+    # Check if the specified user is a member of the specified group
+    def membership_state
+    end
+
+    # POST /v1/groups/:group_id/members
+    # POST /v1/groups/members/create
+    # current user joins group
+    # update membership with state joined/active
+    # add current user as group member
+    def join
+    end
+
+    # POST /v1/groups/:group_id/members/:user_id
+    # current user invites other user to own group
+    # create membership with state invited/pending
+    def invite
+    end
+
+    # DELETE /v1/groups/:group_id/members
+    # POST /v1/groups/members/destroy
+    # current user leaves group
+    # update membership with state left
+    # remove current user from group members
+    def leave
+    end
+
+    # DELETE /v1/groups/:group_id/members/:user_id
+    # current user removes other user from own group
+    # update membership with state kicked
+    # remove other user from group members
+    def kick
+    end
+
+    # GET /v1/groups/ownerships/(:user_id)
+    # returns all groups where user is owner
+    # if no user ID is submitted the current user is assumed
+    def ownerships
+    end
+
+    # GET /v1/groups/memberships/(:user_id)
+    # returns all groups where user is member
+    # if no user ID is submitted the current user is assumed
+    def memberships
+    end
+
+    # POST /v1/groups/:group_id/notifications
+    # enables group notifications if current user is group member
+    def enable_notifications
+    end
+
+    # DELETE /v1/groups/:group_id/notifications
+    # disables group notifications if current user is group member
+    def disable_notifications
     end
 
     private def group_create_params
