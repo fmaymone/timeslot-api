a93124175f3d6bd861937a7b7fe04555ba17029c
silvio <silvio@timeslot.com>
2014-11-06 10:21:01 +0100

    IMP replace should with expected in model spec

--- spec/models/group_spec.rb
+++ spec/models/group_spec.rb
@@ -11,10 +11,10 @@ RSpec.describe Group, type: :model do
   it { is_expected.to respond_to(:subs_can_post) }
   it { is_expected.to respond_to(:subs_can_invite) }
   it { is_expected.to respond_to(:deleted_at) }
-  it { should belong_to(:owner).inverse_of(:own_groups) }
-  it { should have_one(:image) }
-  it { should have_many(:members).through(:memberships) }
-  it { should have_many(:memberships).inverse_of(:group) }
+  it { is_expected.to belong_to(:owner).inverse_of(:own_groups) }
+  it { is_expected.to have_one(:image) }
+  it { is_expected.to have_many(:members).through(:memberships) }
+  it { is_expected.to have_many(:memberships).inverse_of(:group) }
 
   it { is_expected.to be_valid }
 
