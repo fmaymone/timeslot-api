88be2e39d87fd8543561298b5d8202b1eba1f622
silvio <silvio@timeslot.com>
2014-12-11 15:43:33 +0100

    MOD move some config because broke RAD

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -41,4 +41,8 @@ RSpec.configure do |config|
   # The different available types are documented in the features, such as in
   # https://relishapp.com/rspec/rspec-rails/docs
   config.infer_spec_type_from_file_location!
+
+  # to be able to use route helpers in specs
+  config.include Rails.application.routes.url_helpers
+  Rails.application.routes.default_url_options[:host] = 'example.org'
 end
