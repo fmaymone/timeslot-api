c16213310255d9289760e1e366d81fb4eed5a996
silvio <silvio@timeslot.com>
2014-12-11 12:31:49 +0100

    FIX move iOS routing spec into v1 namespace

---
+++ spec/routing/v1/ios_routing_spec.rb
@@ -0,0 +1,12 @@
+require "rails_helper"
+
+RSpec.describe V1::IosController, type: :routing do
+  describe "routing" do
+
+    it "routes to #clean_db" do
+      expect(
+        get: "v1/ios/clean-db"
+      ).to route_to("v1/ios#clean_db", format: :json)
+    end
+  end
+end
