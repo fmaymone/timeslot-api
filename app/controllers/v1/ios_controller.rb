c20f037487915c0459ddb3fe05cfd22cba7f5aec
silvio <silvio@timeslot.com>
2014-12-15 16:03:49 +0100

    MOD debug ios-db-clean env variable on heroku

--- app/controllers/v1/ios_controller.rb
+++ app/controllers/v1/ios_controller.rb
@@ -2,6 +2,9 @@ module V1
   class IosController < ApplicationController
     # GET /v1/ios/clean-db
     def clean_db
+      p ENV['ENABLE_IOS_DB_CLEAN']
+      p ENV['ENABLE_IOS_DB_CLEAN'].class
+
       if ENV['ENABLE_IOS_DB_CLEAN'] == 'true'
         DatabaseCleaner.clean
         head :ok
