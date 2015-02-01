338ab0425cbce51284b675005db9a4fb6de37eee
silvio <silvio@timeslot.com>
2015-02-01 22:39:09 +0100

    ADD BKD-26 #resolved check if associations deleted

--- config/initializers/activerecord_extensions.rb
+++ config/initializers/activerecord_extensions.rb
@@ -1,30 +1,5 @@
-module PreventDeletion
-  class NotActivated < StandardError; end
-
-  def ts_soft_delete
-    update(deleted_at: Time.zone.now)
-  end
-
-  def destroy
-    prevent_remove
-  end
-
-  def delete
-    prevent_remove
-  end
-
-  private def prevent_remove
-    msg = { self: self }
-    msg.merge!(caller_0: caller[0])
-    msg.merge!(caller_1: caller[1])
-    msg.merge!(caller_2: caller[2])
-    Rails.logger.error msg
-    fail NotActivated, msg
-  end
-end
-
 module ActiveRecord
   class Base
-    include PreventDeletion
+    include TSPreventDeletion
   end
 end
