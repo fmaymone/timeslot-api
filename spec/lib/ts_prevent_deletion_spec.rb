fa430659f65cb3eaf5ecfa6ecf0176009ff17286
silvio <silvio@timeslot.com>
2015-02-02 10:50:53 +0100

    WIP add note to slot in delete prevention spec

--- spec/lib/ts_prevent_deletion_spec.rb
+++ spec/lib/ts_prevent_deletion_spec.rb
@@ -3,7 +3,7 @@ require 'ts_prevent_deletion'
 
 describe TSPreventDeletion do
   let(:slot) { FactoryGirl.create(:std_slot) }
-  let(:slot_with_media) { FactoryGirl.create(:std_slot, :with_media) }
+  let(:slot_with_media) { FactoryGirl.create(:std_slot, :with_media, :with_note) }
 
   it "doesn't fail if all objects with references to" \
      " the item to be deleted are also deleted" do
