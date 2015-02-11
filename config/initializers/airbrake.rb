3acd898dc251ee703461eb47c445285680b8e175
silvio <silvio@timeslot.com>
2015-02-11 02:29:25 +0100

    IMP BKD-52 update airbrake config

--- config/initializers/airbrake.rb
+++ config/initializers/airbrake.rb
@@ -1,6 +1,11 @@
 Airbrake.configure do |config|
   config.api_key = '60a29489047b4ca377db98a076bd38d7'
-  config.ignore_only = []
-  config.development_environments = []
-  config.user_attributes = [:id, :username] # ["id"] also works
+  config.user_attributes = [:id, :username]
+
+  # notify all error types:
+  # config.ignore_only = []
+
+  # enable airbrake for dev environment:
+  # set config.consider_all_requests_local to false in development.rb
+  # config.development_environments = []
 end
