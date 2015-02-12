63cfee37203641b24756ee3f5334a02a0bfba891
silvio <silvio@timeslot.com>
2015-02-12 19:59:28 +0100

    FIX remove example roles

--- app/models/concerns/ts_role.rb
+++ app/models/concerns/ts_role.rb
@@ -1,6 +1,6 @@
 module TS_Role
   def self.included(base)
-    base.enum role: { user: 0, bronze: 1, silver: 2, gold: 3, admin: 4 }
+    base.enum role: { user: 0, webview: 1 }
     base.after_initialize :set_default_role, if: :new_record?
   end
 
