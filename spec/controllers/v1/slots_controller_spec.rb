23e35b90ba6b2ba86d28b8f68f9220aa8b149d59
silvio <silvio@timeslot.com>
2015-02-19 23:44:35 +0100

    FIX wrongly indented spec

--- spec/controllers/v1/slots_controller_spec.rb
+++ spec/controllers/v1/slots_controller_spec.rb
@@ -288,20 +288,20 @@ RSpec.describe V1::SlotsController, type: :controller do
         end
       end
     end
+  end
 
-    describe "POST like" do
-      let(:reslot) { create(:re_slot) }
+  describe "POST like" do
+    let(:reslot) { create(:re_slot, slotter: current_user) }
 
-      it "returns http success" do
+    it "returns http success" do
+      post :add_like, { id: reslot.id }
+      expect(response).to be_success
+    end
+
+    it "creates a new Like" do
+      expect {
         post :add_like, { id: reslot.id }
-        expect(response).to be_success
-      end
-
-      it "creates a new Like" do
-        expect {
-          post :add_like, { id: reslot.id }
-        }.to change(Like, :count).by(1)
-      end
+      }.to change(Like, :count).by(1)
     end
   end
 end
