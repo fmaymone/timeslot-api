b19390d5cef856626288bd1476f80d85ad6b25bb
silvio <silvio@timeslot.com>
2015-02-20 14:07:26 +0100

    ADD some documentation

--- app/models/concerns/ts_role.rb
+++ app/models/concerns/ts_role.rb
@@ -1,4 +1,5 @@
 module TS_Role
+  # http://railsapps.github.io/rails-authorization.html
   def self.included(base)
     base.enum role: { user: 0, webview: 1 }
     base.after_initialize :set_default_role, if: :new_record?
