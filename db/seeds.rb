fa3021b6f200d9b46309fcd9fb379cc7bbe6315a
Silvio <silvio@timeslot.com>
2015-07-07 09:01:21 +0200

    minor layout fix

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,6 +9,5 @@
 Provider.create(name: 'facebook')
 Provider.create(name: 'twitter')
 User.create(username: 'webview', role: 1) unless Rails.env == "test"
-						# produces an auth_token to be 
-						# enviroment variables used in TS_SLOT_WEBSHARING
-						# TODO after fixing tests, create also in test enviroment
+# produces an auth_token to be enviroment variables used in TS_SLOT_WEBSHARING
+# TODO after fixing tests, create also in test enviroment
