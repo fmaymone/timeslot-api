df950f0d680f4250966d252c836581f537cfda76
ts-thomas <thomas@timeslot.com>
2015-07-30 22:16:00 +0200

    ADD column size to sql script

--- db/checksize.sql
+++ db/checksize.sql
@@ -6,13 +6,15 @@ SELECT
     table_name,
     pg_size_pretty(table_size) AS table_size,
     pg_size_pretty(indexes_size) AS indexes_size,
-    pg_size_pretty(total_size) AS total_size
+    pg_size_pretty(total_size) AS total_size,
+    column_size
 FROM (
     SELECT
         table_name,
         pg_table_size(table_name) AS table_size,
         pg_indexes_size(table_name) AS indexes_size,
-        pg_total_relation_size(table_name) AS total_size
+        pg_total_relation_size(table_name) AS total_size,
+        pg_column_size(table_name) AS column_size
     FROM (
         SELECT ('"' || table_schema || '"."' || table_name || '"') AS table_name
         FROM information_schema.tables
