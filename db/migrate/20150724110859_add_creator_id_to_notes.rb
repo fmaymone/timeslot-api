570ab0aa54247bcc02f698cbcab2569d2081a7b7
ts-thomas <thomas@timeslot.com>
2015-08-03 21:23:43 +0200

    MOD migration set default to false for creator_id

--- db/migrate/20150724110859_add_creator_id_to_notes.rb
+++ db/migrate/20150724110859_add_creator_id_to_notes.rb
@@ -1,6 +1,6 @@
 class AddCreatorIdToNotes < ActiveRecord::Migration
   def up
-    add_column :notes, :creator_id, 'bigint', null: false
+    add_column :notes, :creator_id, 'bigint', null: false, default: false
   end
 
   def down
