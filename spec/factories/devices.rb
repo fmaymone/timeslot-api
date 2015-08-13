18c99cb1fc1601f196b9e72e12487e2e4a01221b
ts-thomas <thomas@timeslot.com>
2015-08-13 18:18:15 +0200

    ADD device factory and trait for user

---
+++ spec/factories/devices.rb
@@ -0,0 +1,13 @@
+FactoryGirl.define do
+  sequence(:device_id) { |n| "sn-#{n}34628734108347867654395#{n}" }
+
+  factory :device, class: Device do
+    user
+    device_id
+    system 'ios'
+    version '6.0'
+    token 'a43ea436c1eea1d5ebdcd86f46577d664fd28ce4f716350b9adff279e1bbc2ee'
+    endpoint 'arn:aws:sns:us-west-2:293488554927:endpoint/APNS_SANDBOX/Timeslot-iOS/bac63567-f197-3aa2-8e24-77e567cfa042'
+    push true
+  end
+end
