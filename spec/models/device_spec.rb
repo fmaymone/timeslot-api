4ad501630c2fb1b58cba236b467eb18408ebe2d7
ts-thomas <thomas@timeslot.com>
2015-08-14 16:38:30 +0200

    MOD minor optimizations in the rails specs

--- spec/models/device_spec.rb
+++ spec/models/device_spec.rb
@@ -45,7 +45,7 @@ RSpec.describe Device, type: :model do
 
   describe :device, :vcr do
     let(:user) { create(:user) }
-    let!(:device) {
+    let(:device) {
       attributes_for(:device)
           .extract!(:system,
                     :version,
@@ -83,29 +83,28 @@ RSpec.describe Device, type: :model do
   end
 
   describe :register_endpoint, :vcr do
-    let(:user) { create(:user, :with_device) }
+    let(:user) { create(:user) }
+    let(:device) { create(:device, user: user) }
     let(:token) { '999a077973ae7af48c835e4a15973be1fe7db412945ead30007e31c4b5f33500' }
 
     it "adds a new endpoint to the user" do
       expect {
         device.register_endpoint(token)
-      }.to change(Device, :count).by 1
+      }.to change(device, :endpoint)
     end
 
     context "invalid params" do
-      let!(:device) { create(:device) }
-
       it "doesn't add a new device to the user" do
         expect {
           device.register_endpoint(nil)
-        }.not_to change(Device, :count)
+        }.not_to change(device, :endpoint)
       end
 
       it "doesn't add a new device to the user" do
         device[:system] = nil
         expect {
           device.register_endpoint(token)
-        }.not_to change(Device, :count)
+        }.not_to change(device, :endpoint)
       end
     end
   end
