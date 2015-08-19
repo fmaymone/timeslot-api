83f4ae5fec0b08795195df47d145f5cc5ea2049f
ts-thomas <thomas@timeslot.com>
2015-08-19 17:08:47 +0200

    ADD implementation of the asynchron task 'NotifyJob'

---
+++ app/jobs/notify_job.rb
@@ -0,0 +1,22 @@
+class NotifyJob
+  include SuckerPunch::Job
+  workers 10
+
+  def perform(devices, params)
+    client = Device.create_client
+    # currently it is not possible to bulk multiple notifications into one request to AWS SNS
+    # to handle multiple notifications we need topics and subscriber but this mess things up, cuz
+    # we would need to mirror the most of the logic from backend into an "AWS-topic-model"
+    # https://forums.aws.amazon.com/thread.jspa?messageID=639931#639931
+    devices.each do |device|
+      device.notify(client, params)
+    end
+  rescue Exception => exception
+    Rails.logger.error exception
+    raise exception if Rails.env.test? || Rails.env.development?
+  end
+
+  def perform_later(sec, devices, params)
+    after(sec) { perform(devices, params) }
+  end
+end
