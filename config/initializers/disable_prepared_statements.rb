b6d078a4fb5575d63b91cb0dee680662f7455e02
Silvio <silvio@timeslot.com>
2015-09-03 13:25:58 +0200

    MOD use database.yml to disable prepared statements instead of initialiser

--- config/initializers/disable_prepared_statements.rb
+++
@@ -1,7 +0,0 @@
-# disabling postgres prepared statements as it seems we are biten by
-# https://github.com/rails/rails/pull/17607
-# different ways to disable them, for new lets go with this from:
-# http://stackoverflow.com/a/28682798/531439
-ActiveRecord::Base.establish_connection(
-  ActiveRecord::Base.remove_connection.merge(prepared_statements: false)
-)
