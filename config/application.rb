c8ad31765406227ca538b8bc1be97584ec27b9bd
ts-thomas <thomas@timeslot.com>
2015-09-05 14:37:28 +0200

    ADD post header to CORS

--- config/application.rb
+++ config/application.rb
@@ -45,7 +45,7 @@ module TsRailsBackend
     config.middleware.insert_before 0, "Rack::Cors" do
       allow do
         origins '*'
-        resource '*', :headers => :any, :methods => [:get, :options]
+        resource '*', :headers => :any, :methods => [:get, :post, :options]
       end
     end
   end
