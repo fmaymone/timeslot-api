5016d75d5700aae2b8df657e96ff71e45bf5670c
ts-thomas <thomas@timeslot.com>
2015-09-09 17:02:40 +0200

    ADD push to multiple like users

--- app/models/base_slot.rb
+++ app/models/base_slot.rb
@@ -135,8 +135,9 @@ class BaseSlot < ActiveRecord::Base
   def create_like(user)
     like = Like.find_by(slot: self, user: user) || likes.create(user: user)
     like.update(user: user, deleted_at: nil) if like.deleted_at? # relike after unlike
-    Device.notify_all([self.creator], [ message: "#{user.username} likes your slot",
-                                        slot_id: self.id ])
+    users = [like.slot.creator]
+    Device.notify_all(users, [ message: "#{user.username} likes your slot",
+                               slot_id: self.id ])
   end
 
   def destroy_like(user)
@@ -151,6 +152,11 @@ class BaseSlot < ActiveRecord::Base
       new_comment.slot.comments.each do |comment|
         unless user.id.equal?(comment.user.id)
           users << comment.user unless(users.include?(comment.user))
+        end
+      end
+      likes.each do |like|
+        unless user.id.equal?(like.user.id)
+          users << like.user unless(users.include?(like.user))
         end
       end
       Device.notify_all(users, [ message: I18n.t('notify_create_comment',
