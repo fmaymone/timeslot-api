737b0d2ab483fee1f1cf4fe25e537f1b483fbb64
Silvio <silvio@timeslot.com>
2015-09-21 04:22:28 +0200

    IMP slot query object and exhibitor cleaning up

--- app/exhibitors/slot_paginator.rb
+++ app/exhibitors/slot_paginator.rb
@@ -8,36 +8,4 @@ class SlotPaginator < BasePaginator
     self.moment = moment # a timestamp (point-in-time)
     self.data = data
   end
-
-  def call
-    # self.data = @user.my_slots(status: status, moment: moment)
-    slot_query = SlotQuery::OwnSlots.new(relation: user.std_slots.unscoped)
-    reslot_query = SlotQuery::OwnSlots.new(relation: user.re_slots.unscoped)
-    # slot_query.search.test(self.status, moment, limit, id)
-    # self.data = slot_query.retrieve.test(status, moment, limit, user.id)
-    # self.data = slot_query.paginate limit: 2
-
-    # my_query = slot_query.retrieve(
-      # status: status, moment: moment).union(:all, reslot_query.retrieve(
-                                              # status: status, moment: moment))
-
-    pp "calling #{status}"
-    self.data = slot_query.retrieve(status: status, moment: moment)
-    # my_query = slot_query.retrieve(status: status, moment: moment)
-    # .union(reslot_query.retrieve(status: status, moment: moment))
-
-    # slots_table = StdSlot.unscoped.arel_table
-    # table_alias = slots_table.create_table_alias(my_query, :std_slots)
-    # res = StdSlot.unscoped.from(table_alias)
-    # pp res
-
-      # pp self.data
-    # slot_query.retrieve.try(status.to_sym, moment, 50, user.id)
-    # return query object
-
-    self.data += reslot_query.retrieve(status: status, moment: moment)
-
-    # self.data = res
-    self
-  end
 end
