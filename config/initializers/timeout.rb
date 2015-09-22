2b31be23f28ad6403fb8977827e0aef5ce4719b1
Silvio <silvio@timeslot.com>
2015-09-23 00:23:26 +0200

    FIX need to cast string to int for timeout value

--- config/initializers/timeout.rb
+++ config/initializers/timeout.rb
@@ -1,6 +1,5 @@
 if Rails.env.production?
-  timeout_seconds = ENV['RACK_TIMEOUT'] || 15
-  Rack::Timeout.timeout = timeout_seconds
+  Rack::Timeout.timeout = ENV['RACK_TIMEOUT'].to_i || 15
 else
   Rack::Timeout.timeout = 0
 end
