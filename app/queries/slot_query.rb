374cdc7f59ad2b249d141db0e55c0309916a7cf8
Silvio <silvio@timeslot.com>
2015-09-24 21:17:25 +0200

    MOD not using named parameters in slot query

--- app/queries/slot_query.rb
+++ app/queries/slot_query.rb
@@ -9,23 +9,23 @@ module SlotQuery
 
     def retrieve(status: nil, moment: Time.zone.now)
       return @relation if status == 'all' || status.nil?
-      @relation.where(send status, moment: moment)
+      @relation.where(send status, moment)
     end
 
-    private def now(moment: nil)
-      upcoming(moment: moment).or ongoing(moment: moment)
+    private def now(moment = nil)
+      upcoming(moment).or ongoing(moment)
     end
 
-    private def upcoming(moment: Time.zone.now)
+    private def upcoming(moment = Time.zone.now)
       meta_table[:start_date].gt(moment)
     end
 
-    private def ongoing(moment: Time.zone.now)
+    private def ongoing(moment = Time.zone.now)
       meta_table[:start_date].lteq(moment).and(
         meta_table[:end_date].gteq(moment))
     end
 
-    private def past(moment: Time.zone.now)
+    private def past(moment = Time.zone.now)
       meta_table[:end_date].lteq(moment)
     end
 
