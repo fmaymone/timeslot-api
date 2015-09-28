b745b69589e3b65c1f5595f050500e8459d53eab
ts-thomas <thomas@timeslot.com>
2015-09-28 23:23:53 +0200

    ADD implement abstract class 'activity'

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -1,194 +1,74 @@
-class Activity
-  @@enricher = StreamRails::Enrich.new
-  mattr_reader :enricher
+class Activity < ActiveRecord::Base
 
-  def self.user_feed(user_id, params)
-    feed = StreamRails.feed_manager.get_user_feed(user_id)
-    page = paginate(feed, params)
-    pp enricher.enrich_activities(page)
-  end
+  # Mark this class as abstract (do not map relations)
+  self.abstract_class = true
 
-  def self.news_feed(user_id, params, style = 'aggregated')
-    feed = StreamRails.feed_manager.get_news_feeds(user_id)
-    page = paginate(feed[style.to_sym], params)
+  # Trigger activities to Activity Stream
+  include StreamRails::Activity
+  as_activity
 
-    case style
-      when :flat
-        enricher.enrich_activities(page)
-      else#:aggregated
-        enricher.enrich_aggregated_activities(page)
-    end
-  end
+  # The user who made the update
+  def activity_actor() user.id.to_s end
 
-  def self.notification_feed(user_id, params)
-    feed = StreamRails.feed_manager.get_notification_feed(user_id)
-    page = paginate(feed, params)
-    enricher.enrich_activities(page)
-  end
+  # An activity tag as a verb
+  def activity_verb() self.class.name.downcase end
 
-  def self.slot_as_json(slot, user)
+  # The object which was updated/created
+  def activity_object() slot.id.to_s end
 
-    # basic slot data
-    json = slot.slice(:id,
-                      :title,
-                      :start_date,
-                      :created_at,
-                      :updated_at,
-                      :deleted_at).as_json.transform_keys { |key| key.camelize(:lower) }
+  # The object which includes the update as a target
+  # We can use this to group/aggregate activities by slots
+  def activity_target() slot.owner.id.to_s end
 
-    if slot.open_end == false
-      json['endDate'] = slot.end_date
-    end
+  # The foreign id is required to find activities for
+  # changing we need the user here. If users changes their
+  # visiblity, we have to delete activities from stream.
+  def activity_foreign_id() slot.owner.id.to_s end
 
-    # slot location
-    if slot.location_id.nil? && slot.ios_location_id.nil?
-      location_data = nil
-    elsif slot.location_id.nil?
-      location_data = slot.ios_location
+  # Add extra data to each activity. The data can be hide
+  # from the output when the StreamRails::Enrich is not used.
+  def activity_extra_data
+    case self.class.name
+    when 'Comment'
+      message = I18n.t('notify_create_comment',
+                         name: user.username,
+                         title: slot.meta_slot.title)
+    when 'Like'
+      message = "#{user.username} likes your slot"
     else
-      location_data = slot.location
+      return {}
     end
-
-    if location_data.nil?
-      json['location'] = nil
-    else
-      json['location'] = {
-
-          id: location_data.id,
-          name: (location_data.name.blank? ? nil : location_data.name),
-          thoroughfare: location_data.thoroughfare,
-          subThoroughfare: location_data.sub_thoroughfare,
-          locality: location_data.locality,
-          subLocality: location_data.sub_locality,
-          administrativeArea: location_data.administrative_area,
-          subAdministrativeArea: location_data.sub_administrative_area,
-          postalCode: location_data.postal_code,
-          country: location_data.country,
-          isoCountryCode: location_data.iso_country_code,
-          inLandWater: location_data.in_land_water,
-          ocean: location_data.ocean,
-          areasOfInterest: location_data.areas_of_interest,
-          latitude: location_data.latitude,
-          longitude: location_data.longitude,
-          privateLocation: location_data.private_location,
-
-      }
-    end
-
-    # slot creator
-    json['creator'] = slot.creator.slice(:id,
-                                         :username,
-                                         :created_at,
-                                         :updated_at,
-                                         :deleted_at).as_json.transform_keys { |key| key.camelize(:lower) }
-    json['creator']['image'] = {
-        publicId: (user.image.try(:public_id) ? user.image.public_id : nil),
-        localId: (user.image.try(:local_id) ? user.image.local_id : nil)
+    {
+      # The notification message for the activity
+      message: message,
+      # We store full slot data to the activity stream.
+      # The backend needs no further request on the database.
+      slot: Feed.slot_as_json(slot, user),
+      user: Feed.user_as_json(user)
     }
-
-    # slot notes
-    notes = []
-
-    slot.notes.each do |note|
-
-      notes << {
-
-          id: note.id,
-          title: note.title,
-          content: note.content,
-          localId: note.local_id,
-          createdAt: note.created_at
-      }
-
-    end
-
-    json['notes'] = notes
-
-    # slot media items
-    media = []
-
-    slot.media_items.each do |item|
-
-      media_item = {
-
-          mediaId: item.id,
-          publicId: item.public_id,
-          position: item.position,
-          localId: item.local_id,
-          mediaType: item.media_type,
-          createdAt: item.created_at
-      }
-
-      if item.media_type == 'audio'
-        media_item['duration'] = item.duration
-        media_item['title'] = item.title
-      elsif item.media_type == 'video'
-        media_item['duration'] = item.duration
-        media_item['thumbnail'] = item.thumbnail
-      end
-
-      media << media_item
-    end
-
-    json['media'] = media
-
-    # slot settings
-    json['settings'] = { alerts: slot.creator.alerts(slot) }
-    json['visibility'] = slot.visibility if slot.try(:visibility)
-
-    # slot additionals
-    json['groupId'] = slot.group.id if slot.class < GroupSlot
-
-    if slot.try(:group)
-      json['group'] = { id: slot.group.id }
-    elsif slot.class == ReSlot
-      json['slotter'] = { id: slot.slotter_id }
-      json['parent'] = { id: slot.parent_id }
-      json['visibility'] = slot.parent.try(:visibility)
-    elsif slot.class < StdSlot
-      json['reslotsCounter'] = slot.reslot_count
-    end
-
-    json['likes'] = slot.likes.count
-    json['commentsCounter'] = slot.comments.count
-
-    if slot.share_id.empty?
-      json['shareUrl'] = nil
-    else
-      json['shareUrl'] = "#{ENV['TS_SLOT_WEBSHARING_URL']}#{slot.share_id}"
-    end
-
-    json
   end
 
-  def self.user_as_json(user)
-    json = user.slice(:id,
-                      :username,
-                      :created_at,
-                      :updated_at,
-                      :deleted_at).as_json.transform_keys { |key| key.camelize(:lower) }
-    json['image'] = {
-        publicId: (user.image.try(:public_id) ? user.image.public_id : nil),
-        localId: (user.image.try(:local_id) ? user.image.local_id : nil)
-    }
-
-    json['slotCount'] = user.std_slots.unscoped.count
-    json['reslotCount'] = user.re_slots.count
-    json['friendsCount'] = user.friends.count
-
-    json
+  # Returns an array of user which should also be notified
+  # The official documentation of stream_rails gem is incomplete.
+  # A part how to implement aggregations are missing, that's why
+  # we have to fall back to the plain ruby way which is also compatible.
+  def activity_notify
+    # Collect all related user which should be notified
+    user_ids = [slot.owner.id]
+    user_ids.concat(slot.comments.pluck(:user_id).uniq)
+    user_ids.concat(slot.likes.pluck(:user_id))
+    # Remove the user who did the actual comment
+    user_ids.delete(user.id)
+    user_ids = user_ids.uniq
+    # Maps the aggregated feed instance for each user
+    user_ids.map{|v| StreamRails.client.feed('aggregated', v.to_s)}.concat(
+        # Maps also the notification feed instance for each user (optional)
+        user_ids.map{|v| StreamRails.feed_manager.get_notification_feed(v.to_s)}
+    )
   end
 
-  # Cursor based pagination is a lot faster and supported as well.
-  # To go with cursor based pagination we have to implement exchanging
-  # of page hashes (as cursors) between backend and frontend.
-  def self.paginate(feed, limit: 20, offset: 0, cursor: nil)
-    # Simple pagination via offset and limit
-    feed.get(
-        limit: limit,
-        offset: offset,
-        mark_seen: true
-        #id_lt: cursor
-    )['results']
-  end
+  # This code lets the current user's flat and aggregated feeds
+  # follow the target_user's personal feed:
+  # StreamRails.feed_manager.follow_user(user_id, target_id)
+  # StreamRails.client.feed('aggregated', owner_id).follow('user', foreign_id)
 end
