0d90762c8c515ffad4badadb25322e430b133632
ts-thomas <thomas@timeslot.com>
2015-09-28 20:44:40 +0200

    OPT major optimizations of activity controller

--- app/controllers/v1/activity_controller.rb
+++ app/controllers/v1/activity_controller.rb
@@ -8,8 +8,8 @@ module V1
     def user_feed
       authorize :activity
       return head 422 unless has_allowed_params?
-      feed = StreamRails.feed_manager.get_user_feed(current_user.id)
-      render json: paginate(feed), status: :ok
+      render json: Activity.user_feed(current_user.id, page_params),
+             status: :ok
     end
 
     # GET /v1/users/1/news
@@ -19,42 +19,26 @@ module V1
     def news_feed
       authorize :activity
       return head 422 unless has_allowed_params?
-      feed = StreamRails.feed_manager.get_news_feeds(current_user.id)
-      render json: paginate(feed[params[:style].to_sym]), status: :ok
+      style = params[:style].presence || 'aggregated'
+      render json: Activity.news_feed(current_user.id, page_params, style),
+             status: :ok
     end
 
     # GET /v1/users/1/notification
-    # The notification feed can be used to build notification functionality.
+    # This feed can be used to build notification functionality.
     def notification_feed
       authorize :activity
       return head 422 unless has_allowed_params?
-      feed = StreamRails.feed_manager.get_notification_feed(current_user.id)
-      render json: paginate(feed), status: :ok
+      render json: Activity.notification_feed(current_user.id, page_params),
+             status: :ok
     end
 
-    private def paginate(result)
-      # Simple pagination via offset and limit
-      results = result.get(
-          limit: params[:limit].presence || 20,
-          offset: params[:offset].presence || 0
-      )['results']
-
-      # This feature enriches the activity view
-      StreamRails::Enrich.new.enrich_activities(results)
+    private def page_params
+      params.permit(:limit, :offset, :cursor)
     end
-
-    # Cursor based pagination is a lot faster and supported as well.
-    # To go with cursor based pagination we have to implement exchanging
-    # of page hashes (as cursors) between backend and frontend.
-    # private def cursor(result)
-    #   result[params[:style]].get(limit: params[:limit] || 20,
-    #              id_lt: params[:cursor] || 0)['results']
-    # end
 
     private def has_allowed_params?
-      params.permit(:limit, :offset, :cursor, :style)
-      # Set 'aggregated' view as default
-      (params[:style] ||= 'aggregated').in?(%w(flat aggregated))
+      params[:style].nil? || params[:style].in?(%w(flat aggregated))
     end
   end
 end
