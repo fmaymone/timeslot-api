330d6b2f64e963cb290aae1bcb300dc77fa9ed31
ts-thomas <thomas@timeslot.com>
2015-09-28 20:45:07 +0200

    OPT major optimizations of activity model

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -1,4 +1,30 @@
 class Activity
+  @@enricher = StreamRails::Enrich.new
+  mattr_reader :enricher
+
+  def self.user_feed(user_id, params)
+    feed = StreamRails.feed_manager.get_user_feed(user_id)
+    page = paginate(feed, params)
+    pp enricher.enrich_activities(page)
+  end
+
+  def self.news_feed(user_id, params, style = 'aggregated')
+    feed = StreamRails.feed_manager.get_news_feeds(user_id)
+    page = paginate(feed[style.to_sym], params)
+
+    case style
+      when :flat
+        enricher.enrich_activities(page)
+      else#:aggregated
+        enricher.enrich_aggregated_activities(page)
+    end
+  end
+
+  def self.notification_feed(user_id, params)
+    feed = StreamRails.feed_manager.get_notification_feed(user_id)
+    page = paginate(feed, params)
+    enricher.enrich_activities(page)
+  end
 
   def self.slot_as_json(slot, user)
 
@@ -28,23 +54,23 @@ class Activity
     else
       json['location'] = {
 
-        id: location_data.id,
-        name: (location_data.name.blank? ? nil : location_data.name),
-        thoroughfare: location_data.thoroughfare,
-        subThoroughfare: location_data.sub_thoroughfare,
-        locality: location_data.locality,
-        subLocality: location_data.sub_locality,
-        administrativeArea: location_data.administrative_area,
-        subAdministrativeArea: location_data.sub_administrative_area,
-        postalCode: location_data.postal_code,
-        country: location_data.country,
-        isoCountryCode: location_data.iso_country_code,
-        inLandWater: location_data.in_land_water,
-        ocean: location_data.ocean,
-        areasOfInterest: location_data.areas_of_interest,
-        latitude: location_data.latitude,
-        longitude: location_data.longitude,
-        privateLocation: location_data.private_location,
+          id: location_data.id,
+          name: (location_data.name.blank? ? nil : location_data.name),
+          thoroughfare: location_data.thoroughfare,
+          subThoroughfare: location_data.sub_thoroughfare,
+          locality: location_data.locality,
+          subLocality: location_data.sub_locality,
+          administrativeArea: location_data.administrative_area,
+          subAdministrativeArea: location_data.sub_administrative_area,
+          postalCode: location_data.postal_code,
+          country: location_data.country,
+          isoCountryCode: location_data.iso_country_code,
+          inLandWater: location_data.in_land_water,
+          ocean: location_data.ocean,
+          areasOfInterest: location_data.areas_of_interest,
+          latitude: location_data.latitude,
+          longitude: location_data.longitude,
+          privateLocation: location_data.private_location,
 
       }
     end
@@ -56,8 +82,8 @@ class Activity
                                          :updated_at,
                                          :deleted_at).as_json.transform_keys { |key| key.camelize(:lower) }
     json['creator']['image'] = {
-      publicId: (user.image.try(:public_id) ? user.image.public_id : nil),
-      localId: (user.image.try(:local_id) ? user.image.local_id : nil)
+        publicId: (user.image.try(:public_id) ? user.image.public_id : nil),
+        localId: (user.image.try(:local_id) ? user.image.local_id : nil)
     }
 
     # slot notes
@@ -67,11 +93,11 @@ class Activity
 
       notes << {
 
-        id: note.id,
-        title: note.title,
-        content: note.content,
-        localId: note.local_id,
-        createdAt: note.created_at
+          id: note.id,
+          title: note.title,
+          content: note.content,
+          localId: note.local_id,
+          createdAt: note.created_at
       }
 
     end
@@ -85,12 +111,12 @@ class Activity
 
       media_item = {
 
-        mediaId: item.id,
-        publicId: item.public_id,
-        position: item.position,
-        localId: item.local_id,
-        mediaType: item.media_type,
-        createdAt: item.created_at
+          mediaId: item.id,
+          publicId: item.public_id,
+          position: item.position,
+          localId: item.local_id,
+          mediaType: item.media_type,
+          createdAt: item.created_at
       }
 
       if item.media_type == 'audio'
@@ -132,6 +158,37 @@ class Activity
       json['shareUrl'] = "#{ENV['TS_SLOT_WEBSHARING_URL']}#{slot.share_id}"
     end
 
-    json.transform_keys { |key| key.camelize(:lower) }
+    json
+  end
+
+  def self.user_as_json(user)
+    json = user.slice(:id,
+                      :username,
+                      :created_at,
+                      :updated_at,
+                      :deleted_at).as_json.transform_keys { |key| key.camelize(:lower) }
+    json['image'] = {
+        publicId: (user.image.try(:public_id) ? user.image.public_id : nil),
+        localId: (user.image.try(:local_id) ? user.image.local_id : nil)
+    }
+
+    json['slotCount'] = user.std_slots.unscoped.count
+    json['reslotCount'] = user.re_slots.count
+    json['friendsCount'] = user.friends.count
+
+    json
+  end
+
+  # Cursor based pagination is a lot faster and supported as well.
+  # To go with cursor based pagination we have to implement exchanging
+  # of page hashes (as cursors) between backend and frontend.
+  def self.paginate(feed, limit: 20, offset: 0, cursor: nil)
+    # Simple pagination via offset and limit
+    feed.get(
+        limit: limit,
+        offset: offset,
+        mark_seen: true
+        #id_lt: cursor
+    )['results']
   end
 end
