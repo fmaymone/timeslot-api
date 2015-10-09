748afa4cf0c4ff7abade0a4d14c80d1f147b2a74
ts-thomas <thomas@timeslot.com>
2015-10-09 19:26:27 +0200

    MOD redis initializer

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,6 +1,6 @@
 # Automatically bulk redis data to backup file every 1 hour
 if Rails.env.production?
-  $redis = Redis.new(url: ENV['REDIS_URL'], save: '3600 1')
+  $redis = Redis.new(host: ENV['REDIS_URL'], port: ENV['REDIS_PORT'], save: '3600 1')
 else
   $redis = Redis.new(url: 'redis://localhost:6379', save: '3600 1')
 end
