fcf95bcb55c57783a8beaab62103f9b15e7091c3
Silvio <silvio@timeslot.com>
2015-10-11 13:17:40 +0200

    IMP add config for shoulda-matchers 3.0

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -47,3 +47,21 @@ RSpec.configure do |config|
   config.include Rails.application.routes.url_helpers
   Rails.application.routes.default_url_options[:host] = 'example.org'
 end
+
+# shoulda 3.0 needs this type of config
+Shoulda::Matchers.configure do |config|
+  config.integrate do |with|
+    # Choose a test framework:
+    with.test_framework :rspec
+    # with.test_framework :minitest
+    # with.test_framework :minitest_4
+    # with.test_framework :test_unit
+
+    # Choose one or more libraries:
+    with.library :active_record
+    with.library :active_model
+    # with.library :action_controller
+    # Or, choose the following (which implies all of the above):
+    # with.library :rails
+  end
+end
