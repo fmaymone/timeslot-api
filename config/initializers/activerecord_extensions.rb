c60fdaf29f4dfc99eae2159b200a12c281531dc9
Silvio <silvio@timeslot.com>
2015-10-11 18:06:22 +0200

    ADD rspec flag & env var for PG EXPLAIN ANALYZE output

--- config/initializers/activerecord_extensions.rb
+++ config/initializers/activerecord_extensions.rb
@@ -5,31 +5,22 @@ module ActiveRecord
 
   module ConnectionAdapters
     class PostgreSQLAdapter
-      # require 'active_record/connection_adapters/postgresql_adapter'
+      include TSPgExplain
 
-      def __explain_analyze(sql, command, *args)
-        meth = "#{command}_without_explain_analyze".to_sym
-        if /\A\s*SELECT/i.match(sql)
-          newsql = "EXPLAIN (ANALYZE, VERBOSE) #{sql}"
-          # newsql = "EXPLAIN ANALYZE #{sql}"
-          plan = send(meth, newsql, *args).map { |row| row['QUERY PLAN'] }.join("\n")
-          logger = Logger.new(STDOUT)
-          logger.debug("\e[1m\e[31mQUERY PLAN FOR: #{sql.strip};\n#{plan}\e[0m")
-        end
-        send(meth, sql, *args)
-      end
-
-      def execute_with_explain_analyze(sql, *args)
-        __explain_analyze(sql, :execute, *args)
-      end
-
-      def exec_query_with_explain_analyze(sql, *args)
-        __explain_analyze(sql, :exec_query, *args)
-      end
-
-      if Rails.env.development?
+      # to enable explain analyze via rspec tag
+      def self.show_explain
         alias_method_chain :execute, :explain_analyze
         alias_method_chain :exec_query, :explain_analyze
+      end
+
+      def self.hide_explain
+        alias_method :execute, :execute
+        alias_method :exec_query, :exec_query
+      end
+
+      # to enable explain analyze via env var, needs app restart
+      if ENV['PG_EXPLAIN'].nil? ? false : ENV['PG_EXPLAIN'] == 'true'
+        show_explain
       end
     end
   end
