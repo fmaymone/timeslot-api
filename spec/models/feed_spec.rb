0954bc676552845360aacad4aaf6af94f813e0c4
ts-thomas <thomas@timeslot.com>
2015-10-14 23:17:20 +0200

    ADD rails specs for slot feeds

--- spec/models/feed_spec.rb
+++ spec/models/feed_spec.rb
@@ -27,7 +27,6 @@ RSpec.describe Feed, :focus, type: :model do
 
         # user feeds
         user_feed = Feed.user_feed(user.id).as_json
-        pp user_feed
         expect(user_feed.count).to be(6)
 
         user_feed_follower = Feed.user_feed(follower.id).as_json
@@ -38,7 +37,7 @@ RSpec.describe Feed, :focus, type: :model do
 
         # news feeds
         news_feed = Feed.news_feed(user.id).as_json
-        expect(news_feed.count).to be(2)
+        expect(news_feed.count).to be(0)
 
         news_feed_follower = Feed.news_feed(follower.id).as_json
         expect(news_feed_follower.count).to be(2)
@@ -48,7 +47,7 @@ RSpec.describe Feed, :focus, type: :model do
 
         # notification feeds
         notification_feed = Feed.news_feed(user.id).as_json
-        expect(notification_feed.count).to be(2)
+        expect(notification_feed.count).to be(0)
 
         notification_feed_follower = Feed.news_feed(follower.id).as_json
         expect(notification_feed_follower.count).to be(2)
@@ -89,6 +88,97 @@ RSpec.describe Feed, :focus, type: :model do
 
         notification_feed_follower = Feed.news_feed(follower.id).as_json
         expect(notification_feed_follower.count).to be(0)
+
+        notification_feed_follower2 = Feed.news_feed(follower2.id).as_json
+        expect(notification_feed_follower2.count).to be(2)
+      end
+    end
+  end
+
+  context "Slot feeds", :redis do
+    let(:user) { create(:user, :with_email, :with_password, :with_feed) }
+    let(:slot) { create(:std_slot_public, owner: user) }
+
+    before(:each) do
+      # Create relationships
+      slot.add_follower(follower)
+      slot.add_follower(follower2)
+      follower.add_follower(follower2)
+      # Perform activities
+      slot.create_comment(user, 'This is a test comment.')
+      slot.create_like(user)
+      slot.create_comment(follower, 'This is a test comment.')
+      slot.create_like(follower)
+      slot.create_comment(follower2, 'This is a test comment.')
+      slot.create_like(follower2)
+    end
+
+    describe "User follows a slot" do
+      it "User is subscribed to a slot" do
+
+        # user feeds
+        user_feed = Feed.user_feed(user.id).as_json
+        expect(user_feed.count).to be(6)
+
+        user_feed_follower = Feed.user_feed(follower.id).as_json
+        expect(user_feed_follower.count).to be(2)
+
+        user_feed_follower2 = Feed.user_feed(follower2.id).as_json
+        expect(user_feed_follower2.count).to be(2)
+
+        # news feeds
+        news_feed = Feed.news_feed(user.id).as_json
+        expect(news_feed.count).to be(0)
+
+        news_feed_follower = Feed.news_feed(follower.id).as_json
+        expect(news_feed_follower.count).to be(2)
+
+        news_feed_follower2 = Feed.news_feed(follower2.id).as_json
+        expect(news_feed_follower2.count).to be(2)
+
+        # notification feeds
+        notification_feed = Feed.news_feed(user.id).as_json
+        expect(notification_feed.count).to be(0)
+
+        notification_feed_follower = Feed.news_feed(follower.id).as_json
+        expect(notification_feed_follower.count).to be(2)
+
+        notification_feed_follower2 = Feed.news_feed(follower2.id).as_json
+        expect(notification_feed_follower2.count).to be(2)
+      end
+    end
+
+    describe "User unfollows a slot" do
+      it "User is subscribed to a slot" do
+        slot.remove_follower(follower)
+        slot.remove_follower(follower2)
+
+        # user feeds
+        user_feed = Feed.user_feed(user.id).as_json
+        expect(user_feed.count).to be(6)
+
+        user_feed_follower = Feed.user_feed(follower.id).as_json
+        expect(user_feed_follower.count).to be(2)
+
+        user_feed_follower2 = Feed.user_feed(follower2.id).as_json
+        expect(user_feed_follower2.count).to be(2)
+
+        # news feeds
+        news_feed = Feed.news_feed(user.id).as_json
+        expect(news_feed.count).to be(0)
+
+        news_feed_follower = Feed.news_feed(follower.id).as_json
+        expect(news_feed_follower.count).to be(2)
+
+        news_feed_follower2 = Feed.news_feed(follower2.id).as_json
+        expect(news_feed_follower2.count).to be(2)
+
+        # notification feeds
+        notification_feed = Feed.news_feed(user.id).as_json
+        expect(notification_feed.count).to be(0)
+
+        notification_feed_follower = Feed.news_feed(follower.id).as_json
+        expect(notification_feed_follower.count).to be(2)
 
         notification_feed_follower2 = Feed.news_feed(follower2.id).as_json
         expect(notification_feed_follower2.count).to be(2)
