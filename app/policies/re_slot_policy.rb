4ad20849986aa88229d8491a076c18b94f83837a
Silvio <silvio@timeslot.com>
2015-10-25 02:59:07 +0200

    IMP only allow slotter to delete his reslot

--- app/policies/re_slot_policy.rb
+++ app/policies/re_slot_policy.rb
@@ -6,7 +6,7 @@ class ReSlotPolicy < SlotPolicy
   # true if the user is signed in and
   # the user is the slotter
   # is there only one set of media items/notes (those on the parent) for all reslots?
-  # metadata can probably not be updated
+  # TODO: reslot metadata can probably not be updated, add check
   def update_reslot?
     current_user?
   end
@@ -14,6 +14,8 @@ class ReSlotPolicy < SlotPolicy
   # true if the user is signed in and
   # the user is the slotter
   def destroy_reslot?
-    current_user?
+    return false unless current_user?
+    return true if current_user == slot.slotter
+    false
   end
 end
