c01ba3b93182baa5a18525eaeac8b1c3e6fff681
Silvio <silvio@timeslot.com>
2015-10-25 22:36:31 +0100

    DEL remove user agent check code, can't be used with websearch
    code was not running, but now with the web search also using the
    backend this code won’t be used any time soon if at all. removed it.

--- spec/requests/v1/users_spec.rb
+++ spec/requests/v1/users_spec.rb
@@ -14,31 +14,6 @@ RSpec.describe "V1::Users", type: :request do
     end
   end
 
-  describe "User_agent" do
-    context "is an IOS device" do
-      let(:auth_header) do
-        { "HTTP_USER_AGENT" =>
-          "Mozilla/5.0 (iPhone; U; CPU iPhone OS 4_0 like Mac OS X; en-us) AppleWebKit/532.9 (KHTML, like Gecko) Version/4.0.5 Mobile/8A293 Safari/6531.22.7" }
-      end
-      it "is not forbidden" do
-        allow(Rails).to receive(:env).and_return(ActiveSupport::StringInquirer.new("production"))
-        get "/v1/users", {}, auth_header
-        expect(response.status).not_to be 403
-      end
-    end
-
-    context "is not an IOS device" do
-      let(:auth_header) do
-        {"HTTP_USER_AGENT" => "Mozilla/5.0 (compatible; MSIE 10.0; Windows NT 6.2; Trident/6.0)"}
-      end
-      #it "is forbidden" do
-      #  allow(Rails).to receive(:env).and_return(ActiveSupport::StringInquirer.new("production"))
-      #  get "/v1/users", {}, auth_header
-      #  expect(response.status).to be 403
-      #end
-    end
-  end
-
   describe "GET /v1/user/:id" do
     context "with valid ID" do
       it "returns the requested user" do
