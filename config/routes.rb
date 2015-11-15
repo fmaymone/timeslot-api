fa71ff3ce79ed6db7af8b8903cab224ba795d4b9
Silvio <silvio@timeslot.com>
2015-11-15 19:31:53 +0100

    WIP duplicate me-routes from user-routes

--- config/routes.rb
+++ config/routes.rb
@@ -69,6 +69,16 @@ Rails.application.routes.draw do
     get 'media-signature', to: 'media#create_signature'
 
     scope :me do
+      get '', to: 'me#show', as: 'show_me'
+      patch '', to: 'me#update', as: 'update_me'
+      delete '', to: 'me#destroy', as: 'delete_me'
+      get 'slots', to: 'me#slots', as: 'my_slots'
+      get 'friendslots', to: 'me#slots_from_friends', as: 'my_friendslots'
+      get 'media', to: 'me#media_items', as: 'my_media'
+      get 'signout', to: 'me#signout', as: 'sign_me_out'
+      post 'add_friends', to: 'me#add_friends', as: 'add_friends'
+      post 'remove_friends', to: 'me#remove_friends', as: 'remove_friends'
+      patch 'device', to: 'me#update_device', as: 'update_device'
     end
 
     scope :users do
@@ -85,13 +95,14 @@ Rails.application.routes.draw do
           to: 'users#media_items',
           as: 'user_media',
           constraints: { id: /\d+/ }
+      # TODO: remove depricated routes:
       get 'friendslots', to: 'users#slots_from_friends', as: 'user_friendslots'
       patch '', to: 'users#update', as: 'user_update'
       delete '', to: 'users#destroy', as: 'user_delete'
       get 'signout', to: 'users#signout', as: 'signout'
-      post 'add_friends', to: 'users#add_friends', as: 'add_friends'
-      post 'remove_friends', to: 'users#remove_friends', as: 'remove_friends'
-      patch 'device', to: 'users#update_device', as: 'update_device'
+      post 'add_friends', to: 'users#add_friends', as: 'add_friends_old'
+      post 'remove_friends', to: 'users#remove_friends', as: 'remove_friends_old'
+      patch 'device', to: 'users#update_device', as: 'update_device_old'
     end
 
     scope :groups do
--- spec/routing/v1/me_routing_spec.rb
+++ spec/routing/v1/me_routing_spec.rb
@@ -2,4 +2,50 @@ require "rails_helper"
 
 RSpec.describe V1::MeController, type: :routing do
   describe "routing" do
+
+    it "routes to #show" do
+      expect(get: "/v1/me")
+        .to route_to("v1/me#show", format: :json)
+    end
+
+    it "routes to #signout" do
+      expect(get: "/v1/me/signout")
+        .to route_to("v1/me#signout", format: :json)
+    end
+
+    it "routes to #update" do
+      expect(patch: "/v1/me")
+        .to route_to("v1/me#update", format: :json)
+    end
+
+    it "routes to #destroy" do
+      expect(delete: "/v1/me")
+        .to route_to("v1/me#destroy", format: :json)
+    end
+
+    it "routes to #media" do
+      expect(get: "/v1/me/media")
+        .to route_to("v1/me#media_items", format: :json)
+    end
+
+    it "routes to #slots" do
+      expect(get: "/v1/me/slots")
+        .to route_to("v1/me#slots", format: :json)
+    end
+
+    it "routes to #slots_from_friends" do
+      expect(get: "/v1/me/friendslots")
+        .to route_to("v1/me#slots_from_friends", format: :json)
+    end
+
+    it "routes to #add_friends" do
+      expect(post: "/v1/me/add_friends")
+        .to route_to("v1/me#add_friends", format: :json)
+    end
+
+    it "routes to #remove_friends" do
+      expect(post: "/v1/me/remove_friends")
+        .to route_to("v1/me#remove_friends", format: :json)
+    end
+  end
 end
