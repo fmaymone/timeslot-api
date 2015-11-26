98fc63dcf733e8bbfaec4aad7b133715580b302a
ts-thomas <thomas@timeslot.com>
2015-11-26 17:36:28 +0100

    MOD improve aggregation test

--- spec/models/feed_spec.rb
+++ spec/models/feed_spec.rb
@@ -337,13 +337,15 @@ RSpec.describe Feed, :activity, :async, type: :model do
     end
   end
 
-  context "Mixed feeds", :redis do
+  context "Mixed feeds & Aggregation", :redis do
     let(:user) { create(:user) }
     let(:meta_slot) { create(:meta_slot, creator: user) }
     let!(:slot) { create(:std_slot_public, meta_slot: meta_slot) }
     let(:group_meta_slot) { create(:meta_slot, creator: user) }
     let(:group) { create(:group, owner: user) }
     let!(:group_slot) { create(:group_slot, meta_slot: group_meta_slot, group: group) }
+    let!(:reslot_1) { create(:re_slot, predecessor: slot,  parent: slot, slotter: follower) }
+    let!(:reslot_2) { create(:re_slot, predecessor: reslot_1,  parent: slot, slotter: follower2) }
 
     context "User follows mixed type of targets" do
       before(:each) do
@@ -353,6 +355,9 @@ RSpec.describe Feed, :activity, :async, type: :model do
         # Create relationships (Slot-related)
         follower.follow(slot)
         follower2.follow(slot)
+        # Create relationships (ReSlot-related, Crossed)
+        follower2.follow(reslot_1)
+        user.follow(reslot_2)
         # Create relationships (Group-related)
         follower.follow(group)
         follower2.follow(group)
@@ -369,17 +374,33 @@ RSpec.describe Feed, :activity, :async, type: :model do
         # Perform activities (Group)
         group_slot.create_comment(follower, 'This is a test comment.')
         group_slot.create_like(follower)
+
+        # Perform activities (ReSlot 1)
+        reslot_1.create_like(user)
+        reslot_1.create_comment(user, 'This is a test comment.')
+        reslot_1.create_like(follower)
+        reslot_1.create_comment(follower, 'This is a test comment.')
+        reslot_1.create_like(follower2)
+        reslot_1.create_comment(follower2, 'This is a test comment.')
+
+        # Perform activities (ReSlot 2)
+        reslot_2.create_like(user)
+        reslot_2.create_comment(user, 'This is a test comment.')
+        reslot_2.create_like(follower)
+        reslot_2.create_comment(follower, 'This is a test comment.')
+        reslot_2.create_like(follower2)
+        reslot_2.create_comment(follower2, 'This is a test comment.')
       end
 
       it "User Feed (me activities)" do
         user_feed = Feed.user_feed(user.id).as_json
-        expect(user_feed.count).to be(2) # +2 own activities
+        expect(user_feed.count).to be(4) # +4 own activities
 
         user_feed_follower = Feed.user_feed(follower.id).as_json
-        expect(user_feed_follower.count).to be(4) # +4 own activities
+        expect(user_feed_follower.count).to be(7) # +7 own activities
 
         user_feed_follower2 = Feed.user_feed(follower2.id).as_json
-        expect(user_feed_follower2.count).to be(2) # +2 own activities
+        expect(user_feed_follower2.count).to be(5) # +5 own activities
         expect(user_feed_follower).not_to eq(user_feed_follower2)
       end
 
@@ -397,7 +418,7 @@ RSpec.describe Feed, :activity, :async, type: :model do
 
       it "Notification Feed (activities to own contents)" do
         notification_feed = Feed.notification_feed(user.id).as_json
-        expect(notification_feed.count).to be(6) # +6 foreign activities to own content
+        expect(notification_feed.count).to be(12) # +12 foreign activities to own content
 
         notification_feed_follower = Feed.notification_feed(follower.id).as_json
         expect(notification_feed_follower.count).to be(0) # has no own content
@@ -436,10 +457,10 @@ RSpec.describe Feed, :activity, :async, type: :model do
         expect(user_feed.count).to be(1) # +1 new activities
 
         user_feed_follower = Feed.user_feed(follower.id).as_json
-        expect(user_feed_follower.count).to be(2) # +2 new activities
+        expect(user_feed_follower.count).to be(3) # +3 new activities
 
         user_feed_follower2 = Feed.user_feed(follower2.id).as_json
-        expect(user_feed_follower2.count).to be(1) # +1 new activity
+        expect(user_feed_follower2.count).to be(2) # +2 new activity
         expect(user_feed_follower).not_to eq(user_feed_follower2)
       end
 
@@ -457,7 +478,7 @@ RSpec.describe Feed, :activity, :async, type: :model do
 
       it "Notification Feed (activities to own contents)" do
         notification_feed = Feed.notification_feed(user.id).as_json
-        expect(notification_feed.count).to be(3) # +3 foreign activities to own content
+        expect(notification_feed.count).to be(5) # +5 foreign activities to own content
 
         notification_feed_follower = Feed.notification_feed(follower.id).as_json
         expect(notification_feed_follower.count).to be(0) # has no own content
