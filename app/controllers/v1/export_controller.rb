762b9e64ba7c6126473492017d5fa3e8f3471a3c
ts-thomas <thomas@timeslot.com>
2016-01-16 15:25:04 +0100

    Add new export controller

---
+++ app/controllers/v1/export_controller.rb
@@ -0,0 +1,16 @@
+module V1
+  class ExportController < ApplicationController
+
+    def ical
+      # TODO
+    end
+
+    def google
+      # TODO
+    end
+
+    def backup
+      # TODO
+    end
+  end
+end
