6d5b8584f28e974159553e727042e745fdadcb62
ts-thomas <thomas@timeslot.com>
2016-01-16 15:26:35 +0100

    Add model "export"

---
+++ app/models/concerns/share/export.rb
@@ -0,0 +1,22 @@
+module Export
+
+  @storage = RedisStorage
+
+  class << self
+
+    def export_ical
+      # TODO
+    end
+
+    def export_google
+      # TODO
+    end
+
+    def export_backup
+      # TODO
+    end
+  end
+
+  # module ClassMethods
+  # end
+end
