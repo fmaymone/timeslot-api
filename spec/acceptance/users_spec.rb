7efeba807175ab41a08bfc0b6d3f942ae9867d94
Silvio <silvio@timeslot.com>
2016-01-19 11:22:37 +0100

    MOD use constant instead of hard-coded value for PAGINATION_MAX_LIMIT

--- spec/acceptance/users_spec.rb
+++ spec/acceptance/users_spec.rb
@@ -489,7 +489,7 @@ resource "Users" do
           expect(json['paging']).to have_key('limit')
           expect(json['paging']['filter']).to be nil
           expect(json['paging']['after']).to be nil
-          expect(json['paging']['limit']).to eq 40
+          expect(json['paging']['limit']).to eq PAGINATION_MAX_LIMIT
           expect(response_body).to include(re_slots.first.title)
           expect(response_body).to include(re_slots.last.title)
 
