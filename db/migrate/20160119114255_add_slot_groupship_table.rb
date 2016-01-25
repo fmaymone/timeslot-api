159dada6a745f2a374a8941a1f3fa15e59190013
Silvio <silvio@timeslot.com>
2016-01-25 13:15:08 +0100

    MOD fwiw make migration reversible

--- db/migrate/20160119114255_add_slot_groupship_table.rb
+++ db/migrate/20160119114255_add_slot_groupship_table.rb
@@ -1,5 +1,5 @@
 class AddSlotGroupshipTable < ActiveRecord::Migration
-  def change
+  def up
     create_table :slotgroupship do |t|
       t.bigint :slot_id, null: false
       t.bigint :group_id, null: false
@@ -7,5 +7,9 @@ class AddSlotGroupshipTable < ActiveRecord::Migration
       t.timestamps null: false
     end
     change_column :slotgroupship, :id, :bigint
+  end
+
+  def down
+    drop_table :slotgroupship
   end
 end
