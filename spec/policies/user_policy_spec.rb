2c52990f6cbc129c8114a334aa36799334b0c7cb
Silvio <silvio@timeslot.com>
2016-01-25 12:52:04 +0100

    ADD policy to only allow seeing friends of befriended users

--- spec/policies/user_policy_spec.rb
+++ spec/policies/user_policy_spec.rb
@@ -32,4 +32,58 @@ describe UserPolicy do
       end
     end
   end
+
+  permissions :friends? do
+    let(:user) { create(:user) }
+
+    context "for yourself" do
+      it "allows access" do
+        expect(subject).to permit(user, user)
+      end
+    end
+
+    context "for a friend" do
+      let(:friend) do
+        friend = create(:user)
+        create(:friendship, :established, user: user, friend: friend)
+        friend
+      end
+
+      it "allows access" do
+        expect(subject).to permit(user, friend)
+      end
+    end
+
+    context "for a foaf" do
+      let(:foaf) do
+        foaf = create(:user)
+        friend = create(:user)
+        create(:friendship, :established, user: user, friend: friend)
+        create(:friendship, :established, user: foaf, friend: friend)
+        foaf
+      end
+
+      it "denies access" do
+        expect(subject).not_to permit(user, foaf)
+      end
+    end
+
+    context "for a stranger" do
+      let(:stranger) { create(:user) }
+
+      it "denies access" do
+        expect(subject).not_to permit(user, stranger)
+      end
+    end
+
+    context 'for a visitor / invalid or missing auth_token' do
+      let(:user) { nil }
+
+      it "raises MissingCurrentUserError" do
+        expect {
+          subject.new(user, User).public_send(:show?)
+        }.to raise_error TSErrors::MissingCurrentUserError
+      end
+    end
+  end
 end
