2f788460f596725a726def4e54f8d4d39fe10026
Silvio <silvio@timeslot.com>
2016-01-25 13:42:31 +0100

    IMP make migrations more robust by precalculating required fields

--- db/migrate/20160107230953_disallow_null_for_type_column_on_base_slot.rb
+++ db/migrate/20160107230953_disallow_null_for_type_column_on_base_slot.rb
@@ -1,5 +1,18 @@
 class DisallowNullForTypeColumnOnBaseSlot < ActiveRecord::Migration
-  def change
+  def up
+    set_type_for_all_slots
     change_column_null :base_slots, :type, false
+  end
+
+  def down
+    change_column_null :base_slots, :type, true
+  end
+
+  private def set_type_for_all_slots
+    puts "setting type for #{BaseSlot.count} existing slots..."
+    BaseSlot.find_each do |slot|
+      slot.update(type: slot.slot_type) if slot.type.nil?
+      puts "#{slot.id} updated (#{slot.type})"
+    end
   end
 end
