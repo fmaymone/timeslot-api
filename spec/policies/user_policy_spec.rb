4671fcab3045fcf00751b0d733090d7115e28cd7
Silvio <silvio@timeslot.com>
2016-01-29 16:58:36 +0100

    MOD show friends to all signed-in users, not only friends

--- spec/policies/user_policy_spec.rb
+++ spec/policies/user_policy_spec.rb
@@ -13,66 +13,12 @@ describe UserPolicy do
     end
   end
 
-  permissions :show? do
+  permissions :show?, :friends? do
     context "for a user" do
       let(:user) { create(:user) }
 
       it "allows access" do
         expect(subject).to permit(user, User)
-      end
-    end
-
-    context 'for a visitor / invalid or missing auth_token' do
-      let(:user) { nil }
-
-      it "raises MissingCurrentUserError" do
-        expect {
-          subject.new(user, User).public_send(:show?)
-        }.to raise_error TSErrors::MissingCurrentUserError
-      end
-    end
-  end
-
-  permissions :friends? do
-    let(:user) { create(:user) }
-
-    context "for yourself" do
-      it "allows access" do
-        expect(subject).to permit(user, user)
-      end
-    end
-
-    context "for a friend" do
-      let(:friend) do
-        friend = create(:user)
-        create(:friendship, :established, user: user, friend: friend)
-        friend
-      end
-
-      it "allows access" do
-        expect(subject).to permit(user, friend)
-      end
-    end
-
-    context "for a foaf" do
-      let(:foaf) do
-        foaf = create(:user)
-        friend = create(:user)
-        create(:friendship, :established, user: user, friend: friend)
-        create(:friendship, :established, user: foaf, friend: friend)
-        foaf
-      end
-
-      it "denies access" do
-        expect(subject).not_to permit(user, foaf)
-      end
-    end
-
-    context "for a stranger" do
-      let(:stranger) { create(:user) }
-
-      it "denies access" do
-        expect(subject).not_to permit(user, stranger)
       end
     end
 
