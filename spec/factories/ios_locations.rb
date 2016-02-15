fa476f1c86b39174fcd7933351b1af37b634c675
Silvio <silvio@timeslot.com>
2016-02-15 11:49:09 +0100

    IMP factory for meta_slot with candy location has ios_location set

--- spec/factories/ios_locations.rb
+++ spec/factories/ios_locations.rb
@@ -4,5 +4,17 @@ FactoryGirl.define do
     latitude '51.234'
     longitude '-11.993'
     creator
+
+    factory :candy_location do
+      uuid "89840bc4-99fc-bc22-4168-285003ecfd12"
+      name "Signal Iduna Park"
+      locality "Dortmund"
+      sub_locality "Mitte"
+      administrative_area "NW"
+      iso_country_code "DE"
+      latitude '51.4940605163574'
+      longitude '7.45179986953735'
+      creator
+    end
   end
 end
