d463162a2607fa1e45b8f191be7126249239ae35
ts-thomas <thomas@timeslot.com>
2016-03-16 16:43:48 +0100

    FIX aggregation action indexing

--- app/models/concerns/feed.rb
+++ app/models/concerns/feed.rb
@@ -311,8 +311,10 @@ module Feed
       aggregated_feed = []
       # The aggregation group index table
       groups = {}
+      # We have to store the last activity of each activity group
+      last_actions = {}
       # The aggregation targets index table (used for collect activities from same type as the last activity)
-      targets = {}
+      #targets = {}
       # The index of the group index table
       index = -1
       # To determine the paging cursor we use a counter
@@ -328,7 +330,7 @@ module Feed
         post = enrich_activity(post)
         # Generates group tag (acts as the aggregation index)
         # NOTE: Currently we aggregate only activities which has the same type as the last activity (on the same target)
-        group = post['group'] = "#{post['target']}:#{post['action']}" ##{post['time']}
+        group = post['group'] = post['target'] ##{post['time']}
         # Get activity actor
         actor = post['actor'].to_i
         # If group exist on this page then aggregate to this group
@@ -337,13 +339,16 @@ module Feed
           current = groups[group]
           # Get current aggregation group feed
           current_feed = aggregated_feed[current]
-          # Update activity count
-          current_feed['activityCount'] += 1
-          # Collect actors as unique
-          current_feed['actors'] << actor unless current_feed['actors'].include?(actor)
-          # TODO: the intersection feature needs further discussion:
-          # Get intersection of actors and the users social context
-          # current_feed['actors'] &= context if context
+          # Skip this part if the aggregation action is not the same
+          if last_actions[group] === post['action']
+            # Update activity count
+            current_feed['activityCount'] += 1
+            # Collect actors as unique
+            current_feed['actors'] << actor unless current_feed['actors'].include?(actor)
+            # TODO: the intersection feature needs further discussion:
+            # Get intersection of actors and the users social context
+            # current_feed['actors'] &= context if context
+          end
           # Increase aggregation counter (inner count)
           aggregation_count += 1
           # Skip counting for cursor and limits
@@ -351,11 +356,13 @@ module Feed
         # If group does not exist, creates a new group for aggregations
         elsif (feed_count - aggregation_count) < limit.to_i
           # Skip if activity is not from type of the last activity which is related to this target
-          next if targets.has_key?(post['target'])
+          #next if targets.has_key?(post['target'])
           # Set a switch to the target map, so we can check if an activity of these target was already aggregated
-          targets[post['target']] = true
+          #targets[post['target']] = true
           # Increment index on each new group (starting from -1)
           current = groups[group] = (index += 1)
+          # Keep the last action to skip other activities on the same aggregation group
+          last_actions[group] = post['action']
           # Set the whole activity object on each new group
           # which takes the last state of all activities
           current_feed = aggregated_feed[current] = post
