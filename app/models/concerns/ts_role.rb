5377334c3ba5432a1331380f4fba98aa272398fc
Silvio <silvio@timeslot.com>
2016-04-10 10:47:58 +0200

    ADD new user role for global_slot categories

--- app/models/concerns/ts_role.rb
+++ app/models/concerns/ts_role.rb
@@ -1,7 +1,13 @@
-module TS_Role
+module TSRole
   # http://railsapps.github.io/rails-authorization.html
   def self.included(base)
-    base.enum role: { basic: 0, webview: 1, crawler_source: 2 }
+    base.enum role:
+                {
+                  basic: 0,
+                  webview: 1,
+                  crawler_source: 2,
+                  global_slot_category: 3
+                }
     base.after_initialize :set_default_role, if: :new_record?
   end
 
