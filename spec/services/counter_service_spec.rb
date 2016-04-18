b23ee3a41e7423e78a8f9e45dba51d7d0dff0c68
Silvio <silvio@timeslot.com>
2016-04-18 14:34:33 +0200

    ADD service method to count the users who can see a slot

--- spec/services/counter_service_spec.rb
+++ spec/services/counter_service_spec.rb
@@ -48,4 +48,80 @@ RSpec.describe CounterService, type: :service do
       end
     end
   end
+
+  describe "number_of_users_who_can_view_the_slot" do
+    context 'public slot' do
+      let(:slot) { create(:std_slot_public) }
+
+      it "returns 'all'" do
+        count = described_class.new.number_of_users_who_can_view_the_slot(slot)
+        expect(count).to eq 'all'
+      end
+    end
+
+    context 'friend-visible slot' do
+      let(:owner) { create(:user, :with_3_friends) }
+      let(:duplicate_user) { create(:user) }
+      let(:duplicate_member) { create(:user) }
+      let(:slot) { create(:std_slot_friends, owner: owner) }
+
+      let!(:group_1) do
+        group_1 = create(:group, :with_3_members)
+        create(:membership, :active, user: slot.owner, group: group_1)
+        create(:membership, :active, user: duplicate_member, group: group_1)
+        create(:membership, :active, user: duplicate_user, group: group_1)
+        create(:containership, slot: slot, group: group_1)
+        group_1
+      end
+
+      let!(:group_2) do
+        group_2 = create(:group)
+        create_list(:membership, 5, :active, group: group_2)
+        create(:membership, :active, user: duplicate_member, group: group_2)
+        create(:containership, slot: slot, group: group_2)
+      end
+
+      let!(:tagged_users) do
+        create_list(:passengership, 2, slot: slot)
+        create(:passengership, user: duplicate_user, slot: slot)
+      end
+
+      it "number of unique group members + number of tagged users + friends" do
+        count = described_class.new.number_of_users_who_can_view_the_slot(slot)
+        expect(count).to eq 18
+      end
+    end
+
+    context 'private slot' do
+      let(:duplicate_user) { create(:user) }
+      let(:duplicate_member) { create(:user) }
+      let(:slot) { create(:std_slot_private) }
+
+      let!(:group_1) do
+        group_1 = create(:group, :with_3_members)
+        create(:membership, :active, user: slot.owner, group: group_1)
+        create(:membership, :active, user: duplicate_member, group: group_1)
+        create(:membership, :active, user: duplicate_user, group: group_1)
+        create(:containership, slot: slot, group: group_1)
+        group_1
+      end
+
+      let!(:group_2) do
+        group_2 = create(:group)
+        create_list(:membership, 5, :active, group: group_2)
+        create(:membership, :active, user: duplicate_member, group: group_2)
+        create(:containership, slot: slot, group: group_2)
+      end
+
+      let!(:tagged_users) do
+        create_list(:passengership, 2, slot: slot)
+        create(:passengership, user: duplicate_user, slot: slot)
+      end
+
+      it "returns number of unique group members + number of tagged users" do
+        count = described_class.new.number_of_users_who_can_view_the_slot(slot)
+        expect(count).to eq 15
+      end
+    end
+  end
 end
