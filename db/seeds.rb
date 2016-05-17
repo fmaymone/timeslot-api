ecfe82f116a1b632ad6fe078078b1857c2b65364
ts-thomas <thomas@timeslot.com>
2016-05-17 14:06:36 +0200

    DEL remove unused users from seed.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,15 +10,17 @@ Provider.create(name: 'facebook')
 Provider.create(name: 'twitter')
 
 # Pseudo-User for the crawler data/slots aka sources
-User.create(username: 'Timeslot Official',
-            role: 1,
-            email: 'info@timeslot.com')
+# User.create(username: 'Timeslot Official',
+#             role: 1,
+#             email: 'info@timeslot.com')
+
 # for imports via web-importer
 User.create(username: 'Web Importer',
             role: 4,
             email: 'web-importer@timeslot.com',
             password: ENV['WEB_IMPORTER_PASSWORD'])
+
 # calendars with global slots
-User.create(username: 'Global Importer',
-            role: 4,
-            email: 'global-importer@timeslot.com')
+# User.create(username: 'Global Importer',
+#             role: 4,
+#             email: 'global-importer@timeslot.com')
